e54d63e8879b4c0c9d5ecf57a25242da9246076d
Emmanuel Ngoufe <emmanuel.ngoufe-matayeso@informatik.hs-fulda.de>
2023-11-18 09:20:16 +0100

    add the first version of menu

--- src/App.js
+++ src/App.js
@@ -2,9 +2,13 @@ import logo from './logo.svg';
 import React, { useEffect, useState } from 'react';
 import Landing from './Components/Landing';
 import './App.css';
+import Menu from './Components/Menu';
 
 function App(porps) {
-    
+    return (
+        
+        <Menu />     
+    );
 }
 
 export default App;
