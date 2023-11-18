560201b8406f4e5d332101bae878d1dfa7bb8bbb
Emmanuel Ngoufe <emmanuel.ngoufe-matayeso@informatik.hs-fulda.de>
2023-11-18 11:17:43 +0100

    add Introduction/welcomme

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React, { useEffect, useState } from 'react';
 import Landing from './Components/Landing';
 import './App.css';
 
+
 function App(porps) {
-    
+    return(
+    <Landing />
+    );
 }
 
 export default App;
--- src/Components/Landing.js
+++ src/Components/Landing.js
@@ -3,9 +3,26 @@ import AllCourses from './Courses/AllCourses';
 import Course from './Courses/Course';
 import Menu from './Menu';
 import Learn from './StudyingMethods/Learn';
-
+import keyboardwhite from '../keyboardwhite.jpg';
 import './Landing.css';
 
-export default function Landing(props) {
 
+export default function Landing(props) {
+    return (
+        <main>
+            <div className='landingText' style={{ backgroundImage: `url(${keyboardwhite})`, backgroundSize: 'cover'}}>
+                <div className='textContainer'>
+                    <h3 className='title'>Herzlich willkommen! </h3>
+                    <p>Wir freuen uns, Sie auf der shortcut Trainer-Plattform begrüßen zu dürfen.
+                        wir bieten Ihnen die einzigartige Möglichkeit,
+                        Ihr Keyboard-Shurtcut für verschiedene Anwendungen auf verschiedenen Betriebssystemen und in verschiedenen Sprachen zu trainieren.
+                    </p>
+                    <p>
+                        Mit einem unseren Abo erhältst du Zugriff auf unserer top-bewerteten Kurse.
+                       Jetzt kostenlos ausprobieren
+                    </p>
+                </div>
+            </div>
+        </main>
+    );
 }
