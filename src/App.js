1f34d96414cea29ac1196c703c4d64078aee0f77
MarlonResler <marlon.resler@gmail.com>
2023-11-20 00:36:45 +0100

    Showing course with title and border

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import React, { useEffect, useState } from 'react';
 import Landing from './Components/Landing';
+import Course from './Components/Courses/Course';
 import './App.css';
 
 function App(porps) {
-    
+    return <Course courseDetails={{courseName: "Kurs Test", courseId: 2}}></Course>
 }
 
 export default App;
