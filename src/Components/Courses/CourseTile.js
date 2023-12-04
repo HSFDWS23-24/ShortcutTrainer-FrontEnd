a0801b9c41ceea265ae2fe85fbda1757ffe7ef56
MarlonResler <marlon.resler@gmail.com>
2023-12-04 01:10:07 +0100

    courseTile to 4/3 aspect ratio and backgroundImage

--- src/Components/Courses/CourseTile.js
+++ src/Components/Courses/CourseTile.js
@@ -4,9 +4,9 @@ import Test from '../StudyingMethods/Test';
 import './CourseTile.css';
 
 export default function Course(props) {
-  return <div className='courseTileContainer'>
-    <h2 className='courseTileTitle'>{props.courseDetails.courseName}</h2>
-    <div className='courseTileIcon'></div>
+  const url = "url(" + props.courseDetails.imageUrl + ")";
+  return <div className='courseTileContainer' style={{backgroundImage: url}}>
+    <h2 className='courseTileTitle'>{props.courseDetails.courseTitle}</h2>
     {/* Progress bar */}
   </div>
 }
--- src/Components/Courses/MyCourses.js
+++ src/Components/Courses/MyCourses.js
@@ -1,17 +1,13 @@
 import React, { useState } from 'react';
 import './MyCourses.css';
 import CourseTile from './CourseTile';
+import api from '../../api/Model';
 
-export default function MyCourses(){
+export default function MyCourses() {
     //Hier sollten die Kurse gefetched werden, die der Nutzer gerade besucht
-    const myCourses = [
-        {courseId: 1, courseName: "Testkurs"},
-        {courseId: 2, courseName: "Testkurs2"},
-        {courseId: 3, courseName: "Testkurs3"},
-        {courseId: 4, courseName: "Testkurs4"},
-    ];
+    const myCourses = api.getAllCourses();
 
-    const courseElements = myCourses.map(e=>{
+    const courseElements = myCourses.map(e => {
         return <CourseTile courseDetails={e}></CourseTile>
     });
 
