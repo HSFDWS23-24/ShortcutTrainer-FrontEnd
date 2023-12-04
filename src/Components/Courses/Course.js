cbcbceefda04dffbaa8ec97404a0461183b9657a
janalo <janalo957@gmail.com>
2023-12-04 19:56:33 +0100

    Keyboard Layout injected

--- src/Components/Courses/Course.js
+++ src/Components/Courses/Course.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Learn from "../StudyingMethods/Learn";
 import Test from "../StudyingMethods/Test";
+import Question from "../Questions/Question" //temporär
 import "./Course.css";
 import { Typography } from "@mui/material";
 import "@material/web/button/elevated-button.js";
@@ -53,6 +54,7 @@ export default function Course(props) {
             </div>
           </div>
         </div>
+        <Question/>
       </main>
     </>
   );
