c227dcb412082f4d53dea444ae77447f34f86858
Philipp Drüke <philipp.drueke@informatik.hs-fulda.de>
2023-12-17 22:24:08 +0100

    added Auth0 Dependency and Auth0 Provider Context

--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const domain = process.env.REACT_AUTH0_DOMAIN;
+const clientId = process.env.REACT_AUTH0_CLIENT_ID;
+
+
 root.render(
   <React.StrictMode>
+    <Auth0Provider>
+      domain={domain}
+      clientId={clientId}
+      redirectUri={window.location.origin}
+    </Auth0Provider>
     <App />
   </React.StrictMode>
 );
