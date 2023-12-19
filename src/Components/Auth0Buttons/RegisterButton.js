8f8f4f147cd3451be84683c08e764633e08afede
justinbert <justin.bertrams@informatik.hs-fulda.de>
2023-12-19 12:27:59 +0100

    Styling angepasst

--- src/Components/Auth0Buttons/RegisterButton.js
+++ src/Components/Auth0Buttons/RegisterButton.js
@@ -1,15 +1,22 @@
+import React from 'react';
 import { useAuth0 } from '@auth0/auth0-react';
 
 const RegisterButton = () => {
-    const { loginWithRedirect, isAuthenticated } = useAuth0();
+    const { loginWithRedirect, isAuthenticated} = useAuth0();
 
     return (
         !isAuthenticated && (
-            <button onClick={() => loginWithRedirect()}>
+            <button 
+                onClick={() => 
+                    loginWithRedirect({
+                        screen_hint: 'signup',
+                    })
+                }
+            >
                 Register
             </button>
         )
-    )
-}
+    );
+};
 
-export default RegisterButton
+export default RegisterButton;
